2d462a29e6f503a1bdc01152c99c8cbc84542fd4
David Park <DavidPark2027@u.northwestern.edu>
2024-12-04 01:50:56 -0600

    Login endpoint complete

---
+++ spotify-insights-server/.bash_history
@@ -0,0 +1,16 @@
+node app.js
+exit
+node app.js
+clear
+exit
+node app.js
+clear
+exit
+node app.js
+exit
+node app.js
+exit
+node app.js
+exit
+node app.js
+exit
--- spotify-insights-server/app.js
+++ spotify-insights-server/app.js
@@ -29,6 +29,8 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
+console.log(CLIENT_ID);
+
 // -----------
 // Main
 // -----------
