17f9a98bab4e402a8af7f47a71e562a0f20994b8
David Park <DavidPark2027@u.northwestern.edu>
2024-12-04 00:59:17 -0600

    Started authorization code up

--- spotify-insights-client/main.py
+++ spotify-insights-client/main.py
@@ -1,4 +1,6 @@
 # 
+# main.py 
+#
 # Client-side Python app for Spotify Insights.
 #
 # Author: David Park
@@ -12,74 +14,18 @@ import logging
 import time
 
 # ----------
-# Web Service Get
-# ----------
-def web_service_get(url):
-  """
-  Submits a GET request to a web service at most 3 times, since 
-  web services can fail to respond e.g. to heavy user or internet 
-  traffic. If the web service responds with status code 200, 400 
-  or 500, we consider this a valid response and return the response.
-  Otherwise we try again, at most 3 times. After 3 attempts the 
-  function returns with the last response.
-  
-  Parameters
-  ----------
-  url: url for calling the web service
-  
-  Returns
-  -------
-  response received from web service
-  """
-
-  try:
-    retries = 0
-    
-    while True:
-      response = requests.get(url)
-        
-      if response.status_code in [200, 400, 500]:
-        #
-        # we consider this a successful call and response
-        #
-        break;
-
-      #
-      # failed, try again?
-      #
-      retries = retries + 1
-      if retries < 3:
-        # try at most 3 times
-        time.sleep(retries)
-        continue
-          
-      #
-      # if get here, we tried 3 times, we give up:
-      #
-      break
-
-    return response
-
-  except Exception as e:
-    print("**ERROR**")
-    logging.error("web_service_get() failed:")
-    logging.error("url: " + url)
-    logging.error(e)
-    return None
-
-# ----------
 # Login 
 # ----------
 def login(baseurl):
     """
-    
     """
+    
     try:
         api = "login/"
     
         url = baseurl + api
         
-        res = web_service_get(url)
+        res = requests.get(url)
         
         # Not a success
         if res.status_code != 200:
--- spotify-insights-server/app.js
+++ spotify-insights-server/app.js
@@ -1,4 +1,6 @@
 //
+// app.js
+// 
 // Express.js and Node.js web service for Spotify Insights.
 //
 // Author: David Park
@@ -7,14 +9,40 @@
 const express = require('express');
 const app = express();
 
-// main()
-app.listen(8080, () => {
-    console.log('SERVER: web service is running, listening on port 8080');
+// Making HTTP requests to Spotify Web API
+const axios = require('axios'); 
+
+// Working with query strings
+const querystring = require('querystring');
+
+// For secure credential management
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = require('./config.js');
+const spotify_db = require('./spotify_db.js');
+
+// 
+// Spotify app credentials 
+//
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const PORT = 8080;
+
+// -----------
+// Main
+// -----------
+app.listen(config.service_port, () => {
+    console.log('SERVER: web service is running, listening on port', config.service_port);
+
+    process.env.AWS_SHARED_CREDENTIALS_FILE = config.spotify_insights_server_config;
 });
 
-//
-// web service functions (API)
-// 
-let login = require('./api_login.js');
-
-app.get('/login', login.get_login);
+// ----------
+// Login
+// ----------
+app.get('/login', (req, res) => {
+    let scope = '';
+    let auth_url = '';
+});
