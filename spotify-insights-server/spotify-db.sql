76ba71884fc3cd751bd1a1c3b0132046eb1fd71f
David Park <DavidPark2027@u.northwestern.edu>
2024-12-06 19:57:16 -0600

    Finished control access to database

--- spotify-insights-server/spotify-db.sql
+++ spotify-insights-server/spotify-db.sql
@@ -13,8 +13,27 @@ CREATE TABLE tokens (
 
 ALTER TABLE tokens AUTO_INCREMENT = 80001;
 
+-- Testing database creation --
 INSERT INTO 
     tokens (access_token, expiration)
     values ('access_token_1', '2024-12-06 15:30:00');
+
+-- SELECT * FROM tokens;
+
+DROP USER IF EXISTS 'spotify-read-only';
+DROP USER IF EXISTS 'spotify-read-write';
+
+CREATE USER 'spotify-read-only' IDENTIFIED BY 'abc123!!';
+CREATE USER 'spotify-read-write' IDENTIFIED BY 'def456!!';
+
+GRANT SELECT, SHOW VIEW ON spotify.*
+    TO 'spotify-read-only';
+GRANT SELECT, SHOW VIEW, INSERT, UPDATE, DELETE, DROP, CREATE, ALTER ON spotify.*
+    TO 'spotify-read-write';
+
+FLUSH PRIVILEGES;
+
+-- Testing users --
+USE spotify;
 
 SELECT * FROM tokens;
